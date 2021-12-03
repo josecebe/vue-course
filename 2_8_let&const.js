a30f8b860127791d3c0da09763155637e8f5f0bf
Jose Cebellán <josecebe@gmail.com>
2021-12-03 21:32:05 +0100

    2.8 Let & Const!

---
+++ 2_8_let&const.js
@@ -0,0 +1,11 @@
+const name = 'Luffy';
+const lastname = 'Monkey D.';
+console.log(lastname, name);
+
+if (true) {
+    const name = 'Zoro';
+    const lastname = 'Roronoa';
+    console.log(lastname, name);
+}
+
+console.log(lastname, name);
