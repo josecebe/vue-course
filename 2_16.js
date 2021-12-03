36321ba707ca665bfd4428ed4b5eb1d58ab4b8e8
Jose Cebellán <josecebe@gmail.com>
2021-12-03 22:25:00 +0100

    2.16 Import & Export

---
+++ 2_16.js
@@ -0,0 +1,5 @@
+import nakamas from "./2_16_nakamas";
+import { ships } from "./2_16_nakamas";
+
+console.log(nakamas);
+console.log(ships);
---
+++ 2_16_nakamas.js
@@ -0,0 +1,18 @@
+const nakamas = [
+    'Luffy',
+    'Zoro',
+    'Nami',
+    'Usopp',
+    'Sanji',
+    'Chopper',
+    'Robin',
+    'Franky',
+    'Brook',
+    'Jinbe'
+];
+
+export const ships = [
+    'Going Merry',
+    'Thousand Sunny'
+]
+export default nakamas;
