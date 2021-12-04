d605d43e016455547d814a1b7b610bf7b7fc0fb5
Jose Cebellán <josecebe@gmail.com>
2021-12-04 12:11:44 +0100

    3.40 Vue.js first steps

--- 02-vue-basis/app.js
+++ 02-vue-basis/app.js
@@ -1,8 +1,38 @@
+const quotes = [
+    { quote: 'The night is darkest just before the dawn. And I promise you, the dawn is coming.', author: 'Harvey Dent, The Dark Knight' },
+    { quote: 'I believe what doesn’t kill you simply makes you, stranger.', author: 'The Joker, The Dark Knight' },
+    { quote: 'Your anger gives you great power. But if you let it, it will destroy you… As it almost did me', author: 'Henri Ducard, Batman Begins' },
+    { quote: 'You either die a hero or live long enough to see yourself become the villain.', author: 'Harvey Dent, The Dark Knight' },
+    { quote: 'If you’re good at something, never do it for free.', author: 'The Joker, The Dark Knight' },
+    { quote: 'Yes, father. I shall become a bat.', author: 'Bruce Wayne/Batman, Batman: Year One' },
+];
+
 const app = Vue.createApp({
-    template: `
-        <h1>Hello, Grand Line!</h1>
-        <p>Welcome to Paradise</p>
-    `
+    //template: `
+    //    <h1>Hello, Grand Line!</h1>
+    //    <p>Welcome to Paradise</p>
+    //`
+
+    data() {
+        return {
+            title: 'Hello, Grand Line!',
+            description: 'Welcome to Paradise',
+            quotes,
+            newQuote: '',
+        }
+    },
+
+    methods: {
+        changeDescription() {
+            this.description = 'Welcome to New World';
+        },
+
+        addQuote() {
+            this.quotes.unshift({
+                quote: this.newQuote,
+            });
+        }
+    }
 });
 
 app.mount('#myApp');
